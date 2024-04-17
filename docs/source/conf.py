e021359e7097f35d8ce21229ee383d5715284c78
Viicos <65306057+Viicos@users.noreply.github.com>
2024-04-17 09:57:47 +0200

    Switch to Furo docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,5 +55,11 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "source_repository": "https://github.com/Viicos/jsonlogic/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+}
