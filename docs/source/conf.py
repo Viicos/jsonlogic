923b858a6f262c58e09901787aa6564e06c7329b
Victorien <65306057+Viicos@users.noreply.github.com>
2024-04-17 20:35:35 +0200

    Switch to Furo docs theme, check docs in CI (#12)
    * Switch to Furo docs theme
    
    * Check docs in CI

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,16 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
 
+linkcheck_retries = 3
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "source_repository": "https://github.com/Viicos/jsonlogic/",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+}
